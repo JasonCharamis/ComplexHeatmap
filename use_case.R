5344bbc2c0fd896fd93c035f9221c8c1a674e82a
Jason Charamis <125705059+JasonCharamis@users.noreply.github.com>
2023-10-09 10:30:31 +0300

    Create use_case.R

---
+++ use_case.R
@@ -0,0 +1,7 @@
+## Use case for complex_heatmap.R
+
+source("complex_heatmap.R")
+
+complex_heatmap("tsv_file_with_counts", reordered_rows = c(2:7,1,8:58),  reordered_cols = c(12,5,3,7,2,1,4,6,8,9,10,11),
+                color_palette = c("azure2","dodgerblue3","dodgerblue4"), head_annotation =  c(MITO = "lightblue", CYP2 = "gold", CYP3 = "green4", CYP4 = "orange2"),
+                row_annotation = TRUE, gaps_row = c(8,10), gaps_col = c(7,14,43))
